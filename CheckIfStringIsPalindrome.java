cb457c0560d4325fe30d6c0142f49943b11d2bef
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-05 00:06:34 +0530

    Add CheckIfStringIsPalindrome.java

---
+++ CheckIfStringIsPalindrome.java
@@ -0,0 +1,12 @@
+public class main{
+    public static boolean isPalindrome(String str){
+        for(int i=0;i<(str.length()/2);i++){
+            if(str.charAt(i)!= str.length()-1-i){
+                return false;
+            }
+        }
+    return true;
+    }
+    public static void main(){
+}
+}
