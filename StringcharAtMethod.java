2e729fc44ad37cd86de4b3f91307dd7816cbab8a
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-05 00:00:05 +0530

    Add StringcharAtMethod.java

---
+++ StringcharAtMethod.java
@@ -0,0 +1,11 @@
+public class main{
+    public static void charAt(String str){
+        for(int i=0; i<str.length();i++){
+            System.out.pringln(str.chatAt(i));
+        }
+    }
+    public static void main(){
+        
+}
+
+}
