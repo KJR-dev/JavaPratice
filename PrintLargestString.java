50d73b1220bebd58fabb057beb38876be014ebc5
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-05 19:16:01 +0530

    Update PrintLargestString.java

--- PrintLargestString.java
+++ PrintLargestString.java
@@ -1,6 +1,19 @@
 public class LargestString{
+    public static String compare(String[] fruits){
+        String largestTemp=fruits[0];
+        for(int i=1;i<fruits.length;i++){
+            if(largestTemp.compareTo(fruits[i])<0){
+                largestTemp= fruits[i];
+            }else if(largestTemp.compareTo(fruits[i])>0){
+                largestTemp= fruits[i];
+            }
+        }
+        return largestTemp;
+    }
 
     public static void main(){
         String fruits[]={"apple","mango","banana"};
+        
+        System.out.println(compare(fruits));
     }
 }
