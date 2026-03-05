c9e03ce40e0d52e6e625bada5a9222c130b8af6e
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-05 18:10:04 +0530

    Add StringCompare.java

---
+++ StringCompare.java
@@ -0,0 +1,11 @@
+public class StringCompare{
+    public static void stringCompair(String str1,String str2, String str3){
+        
+    }
+    public staic void main(){
+        String str1="Jitendra";
+        String str2="Jitendra";
+        String str3=new String("Jitendra");
+        stringCompair(str1,str2,str3);
+    }
+}
