ccaf0ff9546c4c5f86e2e57369636ecd4cf857c7
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-06 23:23:24 +0530

    Update StringcharAtMethod.java

--- StringcharAtMethod.java
+++ StringcharAtMethod.java
@@ -1,11 +1,12 @@
 public class main{
-    public static void charAt(String str){
+    public static void char(String str){
         for(int i=0; i<str.length();i++){
-            System.out.pringln(str.chatAt(i));
+            System.out.pringln(str.charAt(i));
         }
     }
     public static void main(){
-        
-}
+        String str="Jitendra Sahoo";
+        charAt(str);
+    }
 
 }
