7ac083a4f9e6e8e29fe263a0f9645ec63452aa64
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 17:26:21 +0530

    Add PrintSub-Array.java

---
+++ PrintSub-Array.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void subArray(int numbers[]){
+        for(int i=0;i<numbers.length;i++){
+            for(int j=0;j<numbers.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(numbers[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+        subArray(numbers);
+    }
+}
