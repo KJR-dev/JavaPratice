8f10740286e78246cebe26003394bd00dd568628
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 13:11:12 +0530

    Add Solution.java

---
+++ Solution.java
@@ -0,0 +1,27 @@
+public class StringCompress {
+    public static String stringCompress(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 1;
+
+            while (i < str.length() - 1 && ch == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            sb.append(ch);
+            if (count > 1) {
+                sb.append(count);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbcccdd";
+        System.out.println(stringCompress(str));
+    }
+}
