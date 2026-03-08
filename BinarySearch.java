14b06ab1dbcb5d7eee99efc7eb878f20e31c420d
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 13:34:35 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,22 @@
-int main() {
-    std::cout << "Hello World!\n";
+public class Main {
+    public static int binary(int numbers[], int target){
+        int startIndex=0;
+        int endIndex=numbers.length-1;
+        while(startIndex<=endIndex){
+            int midIndex=(startIndex+endIndex)/2;
+            if(target==numbers[midIndex]){
+                return midIndex;
+            }
+            if(target<numbers[midIndex]){
+                endIndex=--midIndex;
+            }else{
+                startIndex=++midIndex;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6,7,8,9};
+        System.out.println(binary(numbers,6));
+    }
 }
