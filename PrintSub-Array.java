39ad6e28ec07a72eaf5633d68fe3fba478a55192
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 17:21:30 +0530

    Update PrintSub-Array.java

--- PrintSub-Array.java
+++ PrintSub-Array.java
@@ -1,17 +1,10 @@
+// "static void main" must be defined in a public class.
 public class Main {
     public static void subArray(int numbers[]){
         
     }
-    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int numbers[]={2,4,6,8,10};
+        subArray(numbers);
     }
 }
-
-// 2, 4, 6, 8, 10
-
-// 2   2,4    2,4,6     2,4,6,8    2,4,6,8,10
-// 4   4,6    4,6,8     4,6,8,10
-// 6   6,8    6,8,10
-// 8   8,10
-// 10
