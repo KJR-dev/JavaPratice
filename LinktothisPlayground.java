f8279c161078fbe0ab4b499d4d77a685b6a649d9
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 17:27:59 +0530

    Update LinktothisPlayground.java

--- LinktothisPlayground.java
+++ LinktothisPlayground.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void subArray(int numbers[]){
-        
+        for(int i=0;i<numbers.length;i++){
+            for(int j=0;j<numbers.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(numbers[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
     }
-    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int numbers[]={2,4,6,8,10};
+        subArray(numbers);
     }
 }
-
-// 2, 4, 6, 8, 10
-
-// 2   2,4    2,4,6     2,4,6,8    2,4,6,8,10
-// 4   4,6    4,6,8     4,6,8,10
-// 6   6,8    6,8,10
-// 8   8,10
-// 10
