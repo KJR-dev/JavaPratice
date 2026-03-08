b9882c330c63889b46145efbfee537c054d5530e
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 19:45:08 +0530

    Update Max-SubarraySum-II(PrifixSum).java

--- Max-SubarraySum-II(PrifixSum).java
+++ Max-SubarraySum-II(PrifixSum).java
@@ -1,7 +1,7 @@
 // "static void main" must be defined in a public class.
 public class Main {
     public static void subArray(int numbers[]){
-        int preArray[]=new Array(numbers.length);
+        int preArray[]=new int[numbers.length];
         preArray[0]=numbers[0];
         for(int i=1;i<numbers.length;i++){
             preArray[i]=preArray[i-1]+preArray[i];
@@ -10,15 +10,17 @@ public class Main {
         int maxSum=Integer.MIN_VALUE;
         int currSum=0;
         for(int i=0;i<numbers.length;i++){
-            for(int j=i;j<numbers.lenght;j++){
+            for(int j=i;j<numbers.length;j++){
                 currSum=i==0?numbers[j]:numbers[j]-numbers[i-1];
+                maxSum=Math.max(maxSum,currSum);
             }
         }
+        System.out.println(maxSum);
     }
     
     public static void main(String[] args) {
         int numbers[]={1,-2,6,-1,3};
-        
+        subArray(numbers);
     }
 }
 
