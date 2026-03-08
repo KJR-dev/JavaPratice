7f8a65703df6abdf2b0268ea0203d8bda1f6337b
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 13:04:33 +0530

    Update StringCompress.java

--- StringCompress.java
+++ StringCompress.java
@@ -1,21 +1,3 @@
-public class StringCompress{
-    public static String stringCompress(String str){
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            int count=1;
-            while (i < str.length()-1 && ch == str.charAt(i + 1)) {
-                count++;
-                i++;
-            }
-            sb.append(ch);
-            sb.append(count);
-        }
-        return sb.toString();
-    }
-
-    public static void main(String[] args){
-        String str="aaabbcccdde";
-        System.out.println(stringCompress(str));
-    }
+int main() {
+    std::cout << "Hello World!\n";
 }
