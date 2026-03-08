b912e321642bfdf7d899afc227bfb325250eff0f
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 19:55:57 +0530

    Update LinktothisPlayground.java

--- LinktothisPlayground.java
+++ LinktothisPlayground.java
@@ -1,19 +1,20 @@
+// "static void main" must be defined in a public class.
 public class Main {
-    public static void maxSubArrayBruteForce(int numbers[]){
-        int maxSum=0;
+    public static void subArray(int numbers[]){
+        int sum=0;
         for(int i=0;i<numbers.length;i++){
-            for(int j=0;j<numbers.length;j++){
-                int sum=0;
-                for(int k=i;k<=j;k++){
-                    sum+=numbers[k];
-                }
-                maxSum=Math.max(maxSum,sum);
+            sum+=numbers[i];
+            if(sum<0){
+                sum=0;
             }
         }
-        System.out.println(maxSum);
+        System.out.print(sum);
     }
     public static void main(String[] args) {
         int numbers[]={1,-2,6,-1,3};
-        maxSubArrayBruteForce(numbers);
+        subArray(numbers);
     }
 }
+
+// 1 -2 6 -1 3
+// 1 0 6 5 8
