ce7bb3eacb4a91d484a5c68545c2d7597b70f9f7
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-08 19:54:41 +0530

    Add Max-SubarraySum-III(Kadane'sAlgorithm).java

---
+++ Max-SubarraySum-III(Kadane'sAlgorithm).java
@@ -0,0 +1,10 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void subArray(){
+        
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,-2,6,-1,3};
+        subArray(numbers);
+    }
+}
