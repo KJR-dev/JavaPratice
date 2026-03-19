2eeb5839a242b8eeba9926e33c4d5d3ec0b98560
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-20 00:25:43 +0530

    Update LinktothisPlayground.java

--- LinktothisPlayground.java
+++ LinktothisPlayground.java
@@ -1,22 +1,19 @@
-public class Bubble{
-    public static void bubbleSort(int numbers[]){
-        for(int i=0;i<numbers.length;i++){
-            for(int j=i;i<numbers.length-i;j++){
-                if(numbers[i]>numbers[j]){
-                    int temp=numbers[i];
-                    numbers[i]=numbers[j];
-                    numbers[j]=temp;
-                }
+public class Main {
+    public static int[] twoSum(int arr[], int target){
+        Map <Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            int findNum=target-arr[i];
+            if(map.containsKey(findNum)){
+                return new int[]{map.get(findNum),i};
             }
+            map.put(findNum,i);
         }
+        return null;
     }
-    public static void main(String[] args){
-        int numbers[]={3,2,5,3,6,8,9};
-        bubbleSort(numbers);
-        for(int i=0;i<numbers.length;i++){
-            System.out.print(numbers[i]);
-        }
+    public static void main(String[] args) {
+        int arr[]={1,4,2,3,6,4,7,8,12};
+        int target=7;
+        int result[]=twoSum(arr,target);
+        System.out.println(result[0]+","+result[1]);
     }
 }
-
-// 3 -> 
