564ddcceef84ddb764c9c34e23b92592ad74413a
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-03-20 00:37:33 +0530

    Update BuyAndSell.java

--- BuyAndSell.java
+++ BuyAndSell.java
@@ -1,3 +1,17 @@
-int main() {
-    std::cout << "Hello World!\n";
+// "static void main" must be defined in a public class.
+public class Main {
+    public static int buySell(int arr[]){
+        int buy=arr[0];
+        int maxProfit=Integer.MIN_VALUE;
+        for(int i=1;i<arr.length;i++){
+            int currProfit=arr[i]-buy;
+            maxProfit=Math.max(maxProfit,currProfit);
+            buy=Math.min(buy,arr[i]);
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,3,6,2,8,1};
+        System.out.println(buySell(arr));
+    }
 }
