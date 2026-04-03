09999261cc614acebced69983d678fff89e95e1e
Jitendra Sahoo <89997536+KJR-dev@users.noreply.github.com>
2026-04-03 15:51:33 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,26 +1,3 @@
-// "static void main" must be defined in a public class.
-public class Main {
-    public static int[][] merge(int arr[][]){
-	int n=arr.length;
-Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-	int start=arr[0][0];
-	Int end=arr[0][1];
-	List<int[]> list =new ArrayList<>();
-for(int i=0; i<n; i++){
-	int start=arr[i][0];
-	int end=arr[i][1];
-	if(s<=end){
-		end=e;
-	}else{
-		list.add(new int[]{start,end});
-		start=s;
-		end=e;
-	}	
-}
-list.add(new int[]{start,end});
-return list.toArray(new int[list.size()][]);
-}
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+int main() {
+    std::cout << "Hello World!\n";
 }
